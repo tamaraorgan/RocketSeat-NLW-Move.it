eae03fe3dc07817b60a47ce0b0480e91bf1b6e1a
Tamara Organ <tamara@organ.dev.br>
2021-02-25 12:23:02 -0400

    Update - 4 dia

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,8 +1,11 @@
 import { Profiler } from 'inspector'
+import { useContext } from 'react';
+import { ChallengesContext } from './../contexts/ChallengesContext';
 
 import styles from '../styles/components/Profile.module.css'
 
 function Profile() {
+  const { level } = useContext(ChallengesContext)
   return (
     <div className={styles.profileContainer}>
       <img
@@ -13,7 +16,7 @@ function Profile() {
         <strong>Tamara Organ</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,6 +5,7 @@ import CompletedChallenges from '../components/CompletedChallenges'
 import Countdown from '../components/Countdown'
 import ChallengeBox from '../components/ChallengeBox'
 import styles from '../styles/pages/Home.module.css'
+import { CountdownProvider } from '../contexts/CountdownContext'
 
 export default function Home() {
   return (
@@ -14,16 +15,18 @@ export default function Home() {
       </Head>
       <div className={styles.container}>
         <ExperienceBar />
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
       </div>
     </>
   )
