76115ae229c8ba4f0635f2af8a6a191b65f854fc
Tamara Organ <tamara@organ.dev.br>
2021-02-28 12:42:44 -0400

    update

--- src/pages/api/authgithub.ts
+++
@@ -1,8 +0,0 @@
-import { NowRequest, NowResponse } from '@vercel/node'
-
-export default (request: NowRequest, response: NowResponse) => {
-  const { user } = request.body
-  console.log(user)
-
-  return response.json(user)
-}
